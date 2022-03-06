22802209283a5644278bbd49ed547a3386866100
lswatson16 <lauralynw16@gmail.com>
2022-03-06 01:35:07 -0500

    Remove console logs, add semicolons

--- src/Traveler.js
+++ src/Traveler.js
@@ -7,28 +7,22 @@ class Traveler {
   }
 
   calcTotalExpensesForYear() {
-    console.log('trips', this.trips)
+    let fee, grandTotal;
     const filteredTripsByYear = this.trips.filter(trip => {
-      return trip.date.includes("2020")
-    })
+      return trip.date.includes("2020");
+    });
 
-    console.log('filtered by year', filteredTripsByYear)
     const result = filteredTripsByYear.reduce((acc, trip) => {
-      let totalFlightCost = trip.estimatedFlightCostPerPerson * trip.travelers
-      console.log('flight cost', totalFlightCost)
-      let totalLodgingCost = trip.estimatedLodgingCostPerDay * trip.duration
-      console.log('lodging cost', totalLodgingCost)
-      let totalCost = totalFlightCost + totalLodgingCost
-      console.log('total', totalCost)
-      return acc += totalCost
-    }, 0)
+      const totalFlightCost = trip.estimatedFlightCostPerPerson * trip.travelers;
+      const totalLodgingCost = trip.estimatedLodgingCostPerDay * trip.duration;
+      const totalCost = totalFlightCost + totalLodgingCost;
+      return acc += totalCost;
+    }, 0);
 
-    console.log('result', result)
-    let fee = result * .10
-    let grandTotal = result + fee
-    console.log('grandTotal', grandTotal)
-    return grandTotal
-  }
-}
+    fee = result * .10;
+    grandTotal = result + fee;
+    return grandTotal;
+  };
+};
 
 export default Traveler;
