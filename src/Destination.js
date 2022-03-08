23cc472b30dc19a2ad91c81bd32f9e8df397ba30
lswatson16 <lauralynw16@gmail.com>
2022-03-08 13:05:28 -0500

    Add Destination class

---
+++ src/Destination.js
@@ -0,0 +1,12 @@
+class Destination {
+  constructor() {
+    this.id = id;
+    this.destination = destination;
+    this.estimatedLodgingCostPerDay = estimatedLodgingCostPerDay;
+    this.estimatedFlightCostPerPerson = estimatedFlightCostPerPerson;
+    this.image = image;
+    this.alt = alt;
+  }
+}
+
+export default Destination;
