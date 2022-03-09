61e3c4fb0b0f6861be44836eff1a6f91142513a7
lswatson16 <lauralynw16@gmail.com>
2022-03-08 20:26:26 -0500

    Remove comments

--- src/scripts.js
+++ src/scripts.js
@@ -1,14 +1,4 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// An example of how you tell webpack to use a CSS (SCSS) file
 import './css/styles.css';
-
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-// import './images/turing-logo.png'
-
-// console.log('This is the JavaScript entry file - your code begins here.');
-
 import { getTraveler, getTrips, getDestinations, addTripRequest } from './apiCalls.js'
 import Traveler from './Traveler.js'
 import TripDestination from '../src/TripDestination';
@@ -32,11 +22,11 @@ function loadTravelerData(id) {
       console.log(data[1])
       console.log(data[2])
 
+      // ****** organize ******
       traveler = new Traveler(data[0].id, data[0].name, data[0].travelerType)
       destinations = data[2].destinations
       domUpdates.updateTitle(traveler.name)
       const travelerTrips = filterTripsByUserId(data[1].trips, data[0].id)
-      // console.log(traveler)
 
       const detailedTrips = findDestinationsByDestId(data[2].destinations, travelerTrips)
       console.log('detailed trips', detailedTrips)
@@ -50,20 +40,16 @@ function loadTravelerData(id) {
       console.log('test travel trips', traveler.trips)
       domUpdates.displayTrips(traveler.trips)
 
-      // update the hardcoded year to pull from current year
+
       var today = new Date();
       var year = today.getFullYear();
       console.log('test', year)
-      // const travelExpense = traveler.calcTotalExpensesForYear('2020')
       const travelExpense = traveler.calcTotalExpensesForYear(year)
       domUpdates.displayTripExpense(travelExpense.toFixed(2))
 
       domUpdates.createDestinationList(data[2].destinations)
     })
 }
-
-// triggers the GET request for the data
-// loadTravelerData(2);
 
 function filterTripsByUserId(trips, travelerId) {
   const filteredTrips = trips.filter(trip => {
@@ -79,7 +65,6 @@ function findDestinationsByDestId(destinations, filteredTrips) {
     })
 
     const alt = foundDestination.alt === undefined ? '' : foundDestination.alt
-
     let tripWithDestinationInfo = {
       image: foundDestination.image,
       alt: alt,
@@ -112,7 +97,6 @@ function getNewTripRequest() {
 		status: "pending",
 		suggestedActivities: []
   }
-  // console.log('new trip>>>>', newTripRequest)
   return newTripRequest
 }
 
@@ -122,8 +106,6 @@ function getEstimatedCost() {
   const totalCost = (foundDestination.estimatedLodgingCostPerDay * trip.duration) + (foundDestination.estimatedFlightCostPerPerson * trip.travelers)
   const fee = totalCost * .10
   const grandTotal = totalCost + fee
-  // console.log('grand total', grandTotal)
-
   const today = getTodaysDate();
   const chosenDestination = destinationList.options[destinationList.selectedIndex].value
 
@@ -160,13 +142,10 @@ function getTodaysDate() {
 }
 
 function addNewTripRequest(e) {
-  // prevents the default behavior of the form
   e.preventDefault();
   domUpdates.resetInnerHTML(estimatedCost)
-
   const newTripRequested = getNewTripRequest()
 
-  // after making the request, check if the request was successful
   addTripRequest(newTripRequested)
     .then(data => {
       console.log(data)
@@ -213,14 +192,3 @@ function hideLogInSection() {
   domUpdates.hideSection(logInSection)
   domUpdates.showSection(userDashboard)
 }
-
-// function filterTrips(trips) {
-//   return trips.filter(trip => {
-//     let date = getTodaysDate()
-//     console.log('testinggg', date)
-//
-//     // return trip.date.includes(date)
-//   })
-// }
-
-// filterTrips(traveler.trips)
